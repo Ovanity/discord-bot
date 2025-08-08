113e99ce2d5c60425550a1c55a137ab00fa052ac
martin <riembault.martin@gmail.com>
2025-08-08 20:15:31 +0200

    Init

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
-import os
 import discord
 from discord.ext import commands
+import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Charge les variables d'environnement depuis .env
+load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
+intents.message_content = True  # ✅ obligatoire si tu veux !ping
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
